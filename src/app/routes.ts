650d2895d66cd3cc04e06d0282894155b61e3c49
NovitskyiDA <demon@active-bridge.com>
2018-07-23 15:28:18 +0300

    added routes

---
+++ src/app/routes.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { TasksComponent } from './tasks/tasks.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'tasks',
+    pathMatch: 'full'
+  },
+  {
+    path: 'tasks',
+    component: TasksComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  providers: []
+})
+export class AppRoutingModule {
+}
