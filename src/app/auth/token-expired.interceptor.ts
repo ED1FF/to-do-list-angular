072cac8b5a50be99d7c91c818b8abdaf60e5c795
ed1ff <edifei13@yandex.ua>
2018-08-02 17:45:35 +0300

    add sign out method

--- src/app/auth/token-expired.interceptor.ts
+++ src/app/auth/token-expired.interceptor.ts
@@ -2,20 +2,18 @@ import { Injectable } from '@angular/core';
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpResponse, HttpErrorResponse } from '@angular/common/http';
 import { AuthService } from './auth.service';
 import { Observable } from 'rxjs/Observable';
-import { Router } from '@angular/router';
 import 'rxjs/add/operator/do';
 
 @Injectable()
 
 export class TokenExpiredInterceptor implements HttpInterceptor {
-  constructor(public auth: AuthService, public router: Router) {}
+  constructor(public auth: AuthService) {}
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
     return next.handle(request).do((event: HttpEvent<any>) => { }, (err: any) => {
       if (err instanceof HttpErrorResponse) {
         if (err.status === 401) {
-          localStorage.clear();
-          this.router.navigate(['/sign_in']);
+          this.auth.signOut();
         }
       }
     });
