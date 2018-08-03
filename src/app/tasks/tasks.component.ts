00d34f16f1329d8346886e918749b18bfbfd7708
ed1ff <edifei13@yandex.ua>
2018-08-03 11:29:55 +0300

    add toolbar component

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
-import { TaskAPI } from '../api/task'
+import { TaskAPI } from '../api/task';
 
 @Component({
   selector: 'app-tasks',
